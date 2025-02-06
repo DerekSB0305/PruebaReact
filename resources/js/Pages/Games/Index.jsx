8ab7962b813f2df81b6f78db05b45a7337fca44a
DerekSB0305 <dereksolorzano2111@gmail.com>
2025-02-06 09:44:05 -0600

    segundo

---
+++ resources/js/Pages/Games/Index.jsx
@@ -0,0 +1,19 @@
+import Card from "@/Components/Card";
+
+export default function Index({games}) {
+    return (
+        <>
+            {games?.map((game) =>(
+                <Card 
+                key = {game.id} 
+                name = {game.name}
+                classification = {game.classification}
+                genre = {game.genre}
+                price = {game.price}
+                />  
+            ))}
+
+            <div>Hay {Object.keys(games).length} juegos en la base de datos </div>
+        </>
+    );
+}
